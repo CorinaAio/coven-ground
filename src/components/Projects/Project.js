1709626840ffb2247274482fb946ecf1b93efec5
Corina Aionitoaie <c.aionitoaie@goldrepublic.com>
2020-01-15 17:21:23 +0100

    added first draft of homepage

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { Link, graphql } from "gatsby";
-import * as PropTypes from "prop-types";
-import Img from "gatsby-image";
-import { injectIntl } from "gatsby-plugin-intl";
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import * as PropTypes from 'prop-types';
+import Img from 'gatsby-image';
+import { injectIntl } from 'gatsby-plugin-intl';
 
 const Project = ({ intl: { formatMessage }, projectData }) => {
-  return (
-    <div class="hex">
-      <a class="hexIn" href="#">
-        <img
-          src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg"
-          alt=""
-        />
-        <h1>This is a title</h1>
-        <p>Some sample text about the article this hexagon leads to</p>
-      </a>
-    </div>
-  );
+	return (
+		<li>
+			<div class="hex">
+				<a class="hexIn" href="#">
+					<img src="https://img.point.pet/images/a0047-000145-56a2bce25f9b58b7d0cdf7cf.jpg" alt="" />
+					<h1 class="project__title">
+						<span>This is a title</span>
+					</h1>
+				</a>
+			</div>
+		</li>
+	);
 };
 
 export default injectIntl(Project);
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,18 +1,16 @@
-import React from "react";
-import { Link, graphql } from "gatsby";
-import * as PropTypes from "prop-types";
-import Img from "gatsby-image";
-import { injectIntl } from "gatsby-plugin-intl";
-import "./projects.scss";
-import Project from "./Project";
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import * as PropTypes from 'prop-types';
+import Img from 'gatsby-image';
+import { injectIntl } from 'gatsby-plugin-intl';
+import './projects.scss';
+import Project from './Project';
 
 const Projects = ({ intl: { formatMessage }, projects }) => {
-  return (
-    <div id="hexGrid">
-      {projects.map(project => (
-        <Project projectData={project} />
-      ))}
-    </div>
-  );
+	return (
+		<ul id="hexGrid" class="clear">
+			{projects.map((project) => <Project projectData={project} />)}
+		</ul>
+	);
 };
 export default injectIntl(Projects);
